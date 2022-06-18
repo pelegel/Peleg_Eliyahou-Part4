004180b5703024703f54a6c1312879874a023490
Peleg <peleg28@gmail.com>
2022-06-19 00:05:20 +0300

    fixed assignment3_2

--- app.py
+++ app.py
@@ -15,15 +15,6 @@ app.config['SESSION_PERMANENT'] = True
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=20)
 
 
-userss = {'user1': {'name': 'Yossi', 'email': 'Yossi@gmail.com', 'password': 'Yossi12345'},
-         'user2':  {'name': 'Dana', 'email': 'Dana@gmail.com', 'password': 'Dana12345'},
-         'user3': {'name': 'Ron', 'email': 'Ron@gmail.com', 'password': 'Ron12345'},
-         'user4': {'name': 'Noa', 'email': 'Noa@gmail.com', 'password': 'Noa12345'},
-         'user5': {'name': 'Shir', 'email': 'Shir@gmail.com', 'password': 'Shir12345'}}
-
-#list of user's details dictionaries
-users_details = list(userss.values())
-
 ###### Components
 ## menu
 from components.menu.menu import menu
@@ -34,10 +25,6 @@ app.register_blueprint(menu)
 @app.route('/')
 def index_func():
     return render_template('Home.HTML')
-
-
-
-
 
 
 ##home
@@ -56,91 +43,13 @@ app.register_blueprint(assignment3_1)
 from pages.assignment3_2.assignment3_2 import assignment3_2
 app.register_blueprint(assignment3_2)
 
-
 ##assignment4
 from pages.assignment4.assignment4 import assignment4
 app.register_blueprint(assignment4)
 
 
 
-@app.route('/assignment3_2',  methods=['GET', 'POST'])
-def assignment3_2_page():
-    #for search
-    if request.method == 'GET':
-        if 'email' in request.args:
-            email = request.args['email']
 
-            if email == '':
-                return render_template('assignment3_2.html',
-                                       users_details=users_details)
-
-            if email in emails:
-                user_index = get_user_index_by_email(email)
-                username = users_details[user_index]['name']
-                user_password = users_details[user_index]['password']
-                return render_template('assignment3_2.html',
-                                       username=username,
-                                       email=email,
-                                       user_password=user_password)
-            else:
-                return render_template('assignment3_2.html',
-                                       message='not found')
-        else:
-            return render_template('assignment3_2.html')
-
-
-    #for log in
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user_index = get_user_index_by_username(username)
-        if username in usernames:
-            pas_in_dict = users_details[user_index]['password']
-            if pas_in_dict == password:
-                session['username'] = username
-                session['loggedin'] = True
-                return render_template('assignment3_2.html',
-                                       message='Logged in successfully!',
-                                       username=username)
-            else:
-                return render_template('assignment3_2.html',
-                                       message='Wrong password')
-        else:
-            return render_template('assignment3_2.html',
-                                   message='Incorrect username')
-    return render_template('assignment3_2.html')
-
-
-
-
-
-# get all the user's emails and usernames list
-emails =[]
-for i in range(len(users_details)):
-    emails.append(users_details[i]['email'])
-
-usernames =[]
-for i in range(len(users_details)):
-    usernames.append(users_details[i]['name'])
-
-
-# functions to get the user's index in the users' dict by email/username
-def get_user_index_by_email(email):
-    for i in range(len(users_details)):
-        if users_details[i]['email'] == email:
-            return i
-
-def get_user_index_by_username(username):
-    for i in range(len(users_details)):
-        if users_details[i]['name'] == username:
-            return i
-
-
-@app.route("/logout/", methods=['POST'])
-def log_out():
-    session['loggedin'] = False
-    session.clear()
-    return redirect(url_for('assignment3_2_page'))
 
 
 #dictionary of the session
--- pages/assignment3_2/assignment3_2.py
+++ pages/assignment3_2/assignment3_2.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask import request, session, jsonify
+from utilities.db_manager import interact_db
 
 
 #assignment3_2 blueprint definition
@@ -7,56 +8,113 @@ assignment3_2 = Blueprint('assignment3_2', __name__,
                           static_folder='static',
                           template_folder='templates')
 
-#
-# #routes
-# @assignment3_2.route('/assignment3_2',  methods=['GET', 'POST'])
-#  def assignment3_2_page():
-#
-#
-#      #for search
-#      if request.method == 'GET':
-#          if 'email' in request.args:
-#              email = request.args['email']
-#
-#              if email == '':
-#                  return render_template('assignment3_2.html',
-#                                         users_details=users_details)
-#
-#              if email in emails:
-#                  user_index = get_user_index_by_email(email)
-#                  username = users_details[user_index]['name']
-#                  user_password = users_details[user_index]['password']
-#                  return render_template('assignment3_2.html',
-#                                         username=username,
-#                                         email=email,
-#                                         user_password=user_password)
-#              else:
-#                  return render_template('assignment3_2.html',
-#                                         message='not found')
-#          else:
-#              return render_template('assignment3_2.html')
-#
-#
-#      #for log in
-#      if request.method == 'POST':
-#          username = request.form['username']
-#          password = request.form['password']
-#          user_index = get_user_index_by_username(username)
-#          if username in usernames:
-#              pas_in_dict = users_details[user_index]['password']
-#              if pas_in_dict == password:
-#                  session['username'] = username
-#                  session['loggedin'] = True
-#                  return render_template('assignment3_2.html',
-#                                         message='Logged in successfully!',
-#                                         username=username)
-#              else:
-#                  return render_template('assignment3_2.html',
-#                                         message='Wrong password')
-#          else:
-#              return render_template('assignment3_2.html',
-#                                     message='Incorrect username')
-#      return render_template('assignment3_2.html')
-#
-#
+
+#routes
+@assignment3_2.route('/assignment3_2',  methods=['GET', 'POST'])
+def assignment3_2_page():
+    users_details_query = "select * from users"
+    users_details = interact_db(users_details_query, query_type='fetch')
+    emails_query = "select email from users"
+    emails_list = interact_db(emails_query, query_type='fetch')
+    emails = []
+
+    for user in emails_list:
+        emails.append(user.email)
+
+
+    #for search
+    if request.method == 'GET':
+        if 'email' in request.args:
+            email = request.args['email']
+
+            #if no email - return all user's details
+            if email == '':
+                return render_template('assignment3_2.html',
+                                       users_details=users_details)
+
+            if email in emails:
+                username = get_username_by_email(email)
+                user_password = get_password_by_email(email)
+                return render_template('assignment3_2.html',
+                                       username=username,
+                                       email=email,
+                                       user_password=user_password)
+            else:
+                return render_template('assignment3_2.html',
+                                       message='not found')
+        else:
+            return render_template('assignment3_2.html')
+
+
+    #for log in
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        query = "select username from users"
+        usernames = interact_db(query, query_type='fetch')
+
+        usernames_list = interact_db(query, query_type='fetch')
+        usernames = []
+
+        for user in usernames_list:
+            usernames.append(user.username)
+
+        if username in usernames:
+            pas_in_dict = get_password_by_username(username)
+            if pas_in_dict == password:
+                session['username'] = username
+                session['loggedin'] = True
+                return render_template('assignment3_2.html',
+                                       message='Logged in successfully!',
+                                       username=username)
+            else:
+                return render_template('assignment3_2.html',
+                                       message='Wrong password')
+        else:
+            return render_template('assignment3_2.html',
+                                   message='Incorrect username')
+    return render_template('assignment3_2.html')
+
+
+
+
+@assignment3_2.route("/logout/", methods=['POST'])
+def log_out():
+    session['loggedin'] = False
+    session.clear()
+    return redirect('/assignment3_2')
+
+
+
+
+
+def get_username_by_email(email):
+    query = "select username from users where email='%s';" % email
+    usernames = interact_db(query, query_type='fetch')
+    for user in usernames:
+        username = user.username
+    return username
+
+
+
+def get_password_by_email(email):
+    query = "select password from users where email='%s';" % email
+    passwords = interact_db(query, query_type='fetch')
+    for user in passwords:
+        password = user.password
+    return password
+
+
+
+
+def get_password_by_username(username):
+    query = "select password from users where username='%s';" % username
+    passwords = interact_db(query, query_type='fetch')
+    for user in passwords:
+        password = user.password
+    return password
+
+
+
 
