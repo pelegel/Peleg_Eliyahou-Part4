14c3324028d58d8fbf6a3fd4874bab55da55d202
Peleg <peleg28@gmail.com>
2022-06-27 00:31:56 +0300

    insert contact form to contact table

--- pages/contact/contact.py
+++ pages/contact/contact.py
@@ -1,6 +1,8 @@
 import express as express
 from flask import Blueprint, render_template, request, flash, redirect, jsonify
 
+from utilities.db_manager import interact_db
+
 times = 0
 is_contact = 0
 
@@ -17,5 +19,19 @@ def contact_page():
 
 
 
+@contact.route('/create_inquire', methods=['GET', 'POST'])
+def create_inquire():
+    username = request.form['username']
+    email = request.form['email']
+    phone = request.form['phone']
+    range = request.form['range']
+    text = request.form['text']
+
+    query = "insert into contact(name, email, phone_num, rate, content) values ('%s','%s','%s','%s','%s')" % (username, email, phone, range, text)
+
+    interact_db(query, query_type='commit')
+    flash('Contact Sent Successfully!', 'success')
+
+    return redirect('/contact')
 
 
--- pages/contact/static/js/contact.js
+++ pages/contact/static/js/contact.js
@@ -4,21 +4,19 @@ var contact = 0;
 function submitForm() {
 
   if (agree.checked == false && times ==0){
-    window.alert("Are you sure you don't want to get promotion emails?"); 
-    times+=1; 
-  } 
+    window.alert("Are you sure you don't want to get promotion emails?");
+    times+=1;
+  }
   else if (times==1){
-    window.alert("Thank you for contacting us!");  
+    window.alert("Thank you for contacting us!");
     times +=1;
     contact = 1;
   }
   else if (agree.checked == true && contact ==0){
-    window.alert("Thank you for contacting us!");  
+    window.alert("Thank you for contacting us!");
     contact = 1;
   }
   else{
     window.alert("You have already submitted the form");
   }
-  document.getElementById("contact_value").innerHTML += contact;
-
 }
